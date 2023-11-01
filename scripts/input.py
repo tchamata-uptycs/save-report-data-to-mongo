557b8c462a9d88424654df701d6bde496c3b8d42
Masabathula V S Raghavendra Rao <masabathularao@uptycs.com>
2023-11-01 21:46:13 +0530

    changed load type lo KubeQuery_and_SelfManaged_LoadTests

--- scripts/input.py
+++ scripts/input.py
@@ -15,7 +15,7 @@ load_type_options = {
                                         'subtypes':['AWS_MultiCustomer','GCP_MultiCustomer','AWS_SingleCustomer'],
                                         'class':cloudquery_child
                                      },
-                        'KubeQuery+SelfManaged':{
+                        'KubeQuery_and_SelfManaged_LoadTests':{
                                         'subtypes':['KubeQuery_SingleCustomer','SelfManaged_SingleCustomer'],
                                         'class':kubequery_child
                                     } 
