bbab531c0e430c298df5202b9e9fd5ae8b264eac
Your Name <you@example.com>
2023-11-02 10:34:44 +0000

    PROG:kubequery and selfmanaged accuracies

--- scripts/kubequery/selfmanaged_accuracy.py
+++ scripts/kubequery/selfmanaged_accuracy.py
@@ -3,15 +3,14 @@ import sys
 sys.path.append('kubequery/') 
 import json
 import paramiko
-from kubequery_configs import *
+from selfmanaged_configs import *
 from fabric import Connection
 from datetime import datetime, timedelta
 from pathlib import Path
 
-# Variables
 PROJECT_ROOT = Path(__file__).resolve().parent
 
-class Kube_Accuracy:
+class SelfManaged_Accuracy:
 
     def __init__(self,start_timestamp,end_timestamp,prom_con_obj,variables):
         self.load_start=start_timestamp
@@ -27,15 +26,12 @@ class Kube_Accuracy:
         self.password  = "abacus"
         self.target_host = "192.168.143.26"
         self.cloud_domain = "alphacentauri"
-        self.data = final_data
         self.expected_data = None
         self.actual_data = dict()
         self.simnodes = sim_nodes
-        self.kubedata = {0:0,1:0,2:0,3:0,5:0,6:0,7:0,8:0,9:0,10:0}
-        self.cvddata = cvd_data
+        self.vsidata = vsi_data
         self.tables = tables
         self.accuracy = dict()
-
 
     def actual_records(self):
         for t in self.tables:
@@ -49,33 +45,15 @@ class Kube_Accuracy:
             res = conn.sudo(command, password=self.password, hide='stderr')
             result_list = res.stdout.split("\n")
             self.actual_data[t] = int(result_list[0].split("\"")[1])
-
         #print(self.actual_data)
-
 
     def expected_records(self):
         for node in self.simnodes:
             ssh_client = paramiko.SSHClient()
             ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh_client.connect(node, self.port, self.username, self.password)
-            command = "cd /home/abacus/kubequerysim/logs && tail -n 11 \"$(ls -lrth | tail -n 1 | awk '{print $9}')\" | awk '{ for (i = 7; i <= NF; i++) printf $i \" \"; printf \"\\n\" }'"
-            #command = "cd /home/abacus/kubequerysim/logs && tail -n 11 \"$(ls -lrth | head -n 60 | tail -n 1 | awk '{print $9}')\" | awk '{ for (i = 7; i <= NF; i++) printf $i \" \"; printf \"\\n\" }'"
-
-            stdin, stdout, stderr = ssh_client.exec_command(command)
-            output = stdout.read().decode('utf-8')
-            errors = stderr.read().decode('utf-8')
-            #print(output)
-        
-            for i,data in enumerate(output.split("\n")):
-                if i in self.kubedata.keys():
-                    self.kubedata[i]+=int(re.findall(r'\d+',data)[0])
-
-            self.kube_data = {kube_index_map[key]: value for key, value in self.kubedata.items()}
-            #print(self.kube_data)
-
-            ssh_client.connect(node, self.port, self.username, self.password)
-
-            command = "cd /home/abacus/new_kubequery && cat osx_log28001.out | grep statistic"
+    
+            command = "cd /home/abacus/vsi_selfmanaged && cat osx_log28001.out | grep statistic"
             stdin, stdout, stderr = ssh_client.exec_command(command)
             output = stdout.read().decode('utf-8')
             errors = stderr.read().decode('utf-8')
@@ -88,15 +66,14 @@ class Kube_Accuracy:
                     match = re.search(pattern, data).group(1)
                     valid_json_string = match.replace(" ",",").replace("{", '{"').replace(":", '":').replace(',',',"')
                     values = json.loads(valid_json_string)
-                    self.cvddata = {key: self.cvddata[key] + values[key] for key in self.cvddata}
+                    self.vsidata = {key: self.vsidata[key] + values[key] for key in self.vsidata}
                 
-        self.cvddata = {key: self.cvddata[key] * asset_count for key in self.cvddata}
-        self.cvddata = {key_mapping[key]: value for key, value in self.cvddata.items()}
-        #print(self.cvddata)
-        self.expected_data = {**self.kube_data, **self.cvddata}
-        print(self.expected_data)
+        self.vsidata = {key: self.vsidata[key] * asset_count for key in self.vsidata}
+        self.expected_data = {key_mapping[key]: value for key, value in self.vsidata.items()}
+            #print(self.cvddata)
+        #print(self.expected_data)
 
-    def accuracy_kubernetes(self):
+    def accuracy_selfmanaged(self):
         self.expected_records()
         self.actual_records()
         for t in self.tables:
@@ -105,12 +82,13 @@ class Kube_Accuracy:
                 "Actual Records" : self.actual_data[t],
                 "Accuracy" : (self.actual_data[t]/self.expected_data[t])*100
             }
-        #print(self.accuracy)
+        print(self.accuracy)
         return self.accuracy
 
 
     
-obj = Kube_Accuracy('2023-10-18 18:45:00','2023-10-19 05:00:00','c','d')   
-obj.accuracy_kubernetes()
+obj = SelfManaged_Accuracy('2023-11-01 18:40:00','2023-11-02 05:05:00','c','d')   
+obj.accuracy_selfmanaged()
+
     
 
